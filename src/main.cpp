43c413d48527d11a99336107161504d201d5e799
Kenneth Browder <pandanautinspace@gmail.com>
2023-08-02 15:31:13 -0400

    improving ui

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 // #include "MMGrid.hpp"
 // #include "PRand.hpp"
 #include "common/SimulatedAnnealing.hpp"
+#include <filesystem>
 
 using namespace std;
 using namespace Eigen;
@@ -27,7 +28,8 @@ void printConstraints(ConstraintGraph cf)
 	cout << "]" << endl;
 }
 
-void main_draw_debug(){
+void main_draw_debug()
+{
 	srand(time(NULL));
 	int rows = 2;
 	int cols = 2;
@@ -260,7 +262,7 @@ void main_draw_debug(){
 				myGrid.update(timeStep / updatesPerRender);
 			}
 		}
-		if(following_path)
+		if (following_path)
 			cout << "PATH ERROR: " << myGrid.getCurrentError() << endl;
 		return false;
 	};
@@ -346,14 +348,16 @@ void main_draw_debug(){
 	viewer.launch();
 }
 
-void main_sa() {
+void main_sa()
+{
 	SimulatedAnnealing sa("../configs/waterdrop.txt");
 	MMGrid mg = sa.simulate(20);
 	ConstraintGraph cg(mg.getRows(), mg.getCols(), mg.getCells());
 	printConstraints(cg);
 }
 
-void main_verify() {
+void main_verify()
+{
 	srand(time(NULL));
 	int rows = 2;
 	int cols = 2;
@@ -365,7 +369,7 @@ void main_verify() {
 
 	MMGrid myGrid(rows, cols, cells);
 	myGrid.loadFromFile("../configs/waterdrop.txt");
-	ConstraintGraph vf({1,2,2,2,2,2}, {1,2,0,2,2,0});
+	ConstraintGraph vf({1, 2, 2, 2, 2, 2}, {1, 2, 0, 2, 2, 0});
 	rows = myGrid.getRows();
 	cols = myGrid.getCols();
 	cells = vf.allConstrainedCells();
@@ -390,6 +394,7 @@ void main_verify() {
 	viewer.plugins.push_back(&plugin);
 	igl::opengl::glfw::imgui::ImGuiMenu menu;
 	plugin.widgets.push_back(&menu);
+	menu.name = "Config Menu";
 
 	int selected_cell = 0;
 	int selected_joint = 0;
@@ -397,7 +402,89 @@ void main_verify() {
 	bool edit_enabled = false;
 	char editMode = 'r';
 	bool following_path = false;
-	string config_file = "";
+	string config_file = std::filesystem::current_path();
+	string path_file = std::filesystem::current_path();
+	string out_config_file = std::filesystem::current_path();
+	string out_angle_file = std::filesystem::current_path();
+	vector<string> paths;
+
+	menu.callback_draw_viewer_menu = [&]()
+	{
+		// menu.draw_viewer_menu();
+
+		if (ImGui::CollapsingHeader("Grid Design", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			ImGui::SliderInt("Row Number", &rows, 2, 10);
+			ImGui::SliderInt("Column Number", &cols, 2, 10);
+			ImGui::Button("Set rows and columns!");
+			if (ImGui::CollapsingHeader("Import Config File"))
+			{
+				ImGui::InputText("Config File", &config_file);
+				if (ImGui::Button("Import Config"))
+				{
+				}
+			}
+			if (ImGui::CollapsingHeader("Export Config File"))
+			{
+				ImGui::InputText("Config File Output", &out_config_file);
+				if (ImGui::Button("Save Config"))
+				{
+				}
+			}
+		}
+
+		if (ImGui::CollapsingHeader("Specify Motion", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			ImGui::Text("Existing Paths:");
+			if (paths.size() > 0)
+			{
+				auto it = paths.begin();
+				while (it != paths.end())
+				{
+					string path = *it;
+					ImGui::Text("%s", path.c_str());
+					ImGui::SameLine();
+					if (ImGui::Button("Delete"))
+					{
+						it = paths.erase(it);
+					};
+				}
+			}
+			else
+			{
+				ImGui::Text("None so far!");
+			}
+			ImGui::InputText("Path File", &path_file);
+			if (ImGui::Button("Add a new path"))
+			{
+				paths.push_back(path_file);
+			}
+		}
+
+		if (ImGui::CollapsingHeader("Optimization", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			if (ImGui::Button("Generate Cell Placement"))
+			{
+			}
+			ImGui::Button("Add Active Cell at Selected");
+			if (ImGui::CollapsingHeader("Export Angle Outputs"))
+			{
+				ImGui::InputText("Angle File Output", &out_angle_file);
+				if (ImGui::Button("Save Angles"))
+				{
+				}
+			}
+		}
+
+		if (ImGui::CollapsingHeader("Simulation", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			if (ImGui::Button("Preview Motion"))
+			{
+				following_path = !following_path;
+				myGrid.setCells(rows, cols, cells);
+			}
+		}
+	};
 
 	menu.callback_draw_custom_window = [&]()
 	{
@@ -504,29 +591,35 @@ void main_verify() {
 			myGrid.setCells(rows, cols, cells);
 		}
 
-		if (ImGui::Button("Print Active Cell Indices")) {
+		if (ImGui::Button("Print Active Cell Indices"))
+		{
 			ConstraintGraph cf(rows, cols, cells);
 			vector<int> indices = cf.getActiveCellIndices(cells);
-			for(int index: indices) {
+			for (int index : indices)
+			{
 				std::cout << index << ",";
 			}
 			std::cout << std::endl;
 		}
 
-		if (ImGui::Button("Print Angles for Active Cell Indices")) {
+		if (ImGui::Button("Print Angles for Active Cell Indices"))
+		{
 			myGrid.setCells(rows, cols, cells);
 			ConstraintGraph cf(rows, cols, cells);
 			vector<int> indices = cf.getActiveCellIndices(cells);
 			std::cout << "Indices: ";
-			for(int index: indices) {
+			for (int index : indices)
+			{
 				std::cout << index << ",";
 			}
-			std::cout<< std::endl;
+			std::cout << std::endl;
 			vector<vector<double>> angles = myGrid.getAnglesFor(indices);
-			for(int i = 0; i < indices.size(); i++) {
+			for (int i = 0; i < indices.size(); i++)
+			{
 				std::cout << "For cell " << indices[i] << "(" << indices[i] / cols << " " << indices[i] % cols << "): " << std::endl;
 				std::cout << "Angles:" << std::endl;
-				for(double d : angles[i]) {
+				for (double d : angles[i])
+				{
 					std::cout << d * 180 / M_PI << std::endl;
 				}
 				std::cout << std::endl;
@@ -619,7 +712,7 @@ void main_verify() {
 				myGrid.update(timeStep / updatesPerRender);
 			}
 		}
-		if(following_path)
+		if (following_path)
 			cout << "PATH ERROR: " << myGrid.getCurrentError() << endl;
 		return false;
 	};
